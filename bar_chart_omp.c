fee5d3e4db0f7dfb2ccb8767888b023000de6e63
Kholodnaf <146763902+Kholodnaf@users.noreply.github.com>
2025-05-02 18:54:58 +0300

    Add files via upload

---
+++ bar_chart_omp.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main() {
+    int size, num_threads;
+    printf("Enter the dataset size: ");
+    scanf("%d", &size);
+
+    int* data = (int*)malloc(size * sizeof(int));
+    printf("Enter the dataset values (separated by spaces): ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &data[i]);
+    }
+
+    printf("Enter number of threads: ");
+    scanf("%d", &num_threads);
+
+    printf("--- Bar chart ---\n");
+
+    int max_value = 0;
+    #pragma omp parallel for reduction(max:max_value) num_threads(num_threads)
+    for (int i = 0; i < size; i++) {
+        if (data[i] > max_value) {
+            max_value = data[i];
+        }
+    }
+
+    
+    #pragma omp parallel for num_threads(num_threads)
+    for (int value = 1; value <= max_value; value++) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (data[i] == value) {
+            count++;
+        }
+    }
+    // Always print the Data Point
+    printf("Data Point %d: ", value);
+    for (int j = 0; j < count; j++) {
+        printf("*");
+    }
+    printf("\n");  // New line after each data point
+}
+
+
+    free(data);
+    return 0;
+}
