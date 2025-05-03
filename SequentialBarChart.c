20e5335b5e8d0092f3741f58f5f94197fc5bf509
LamaCsc <146310402+LamaCsc@users.noreply.github.com>
2025-05-04 02:06:06 +0300

    Create SequentialBarChart.c

---
+++ SequentialBarChart.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to generate and display bar chart
+void generate_bar_chart(int* data, int size) {
+    printf("\n--- Bar chart ---\n");
+    for (int i = 0; i < size; i++) {
+        printf("Data Point %d: ", i + 1);
+        for (int j = 0; j < data[i]; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the dataset size: ");
+    scanf("%d", &size);
+
+    int* data = (int*)malloc(size * sizeof(int));
+    if (!data) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Enter the dataset values (separated by spaces): ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &data[i]);
+    }
+
+    generate_bar_chart(data, size);
+    free(data);
+
+    return 0;
+}
